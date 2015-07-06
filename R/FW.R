c5260a91ef02f5064c777b33e26fa40b80f15624
lian0090 <lian0090@umn.edu>
2015-07-06 15:20:27 -0400

    when Variance matrix is provided, GibbsFW will automaticall use the column names of Variance matrix as VARlevels

--- R/FW.R
+++ R/FW.R
@@ -1,12 +1,12 @@
 #a wrapper for GibbsFW and lmFW
-FW=function(y,VAR,ENV,method=c("OLS","Gibbs")[2], VARlevels=NULL,ENVlevels=NULL,savedir=".",nIter=5000,burnIn=3000,thin=1,df=5,dfg=5,dfh=5,dfb=5,S=NULL,Sg=NULL,Sb=NULL,Sh=NULL,A=NULL,inits=NULL,nchain=1,seed=NULL){
+FW=function(y,VAR,ENV,method=c("OLS","Gibbs")[2], savedir=".",nIter=5000,burnIn=3000,thin=1,df=5,dfg=5,dfh=5,dfb=5,S=NULL,Sg=NULL,Sb=NULL,Sh=NULL,A=NULL,inits=NULL,nchain=1,seed=NULL){
 
 if(method=="OLS"){
-	 predictedValue=lmFW(y,VAR,ENV,VARlevels,ENVlevels,savedir=savediri)
+	 predictedValue=lmFW(y,VAR,ENV,savedir=savedir)
 }
 
 if(method=="Gibbs"){
-	 	 predictedValue=GibbsFW(y=y,VAR=VAR,ENV=ENV,VARlevels=VARlevels,ENVlevels=ENVlevels,nIter=nIter,burnIn=burnIn,thin=thin,A=NULL,seed=seed,savedir=savediri);	
+	 	 predictedValue=GibbsFW(y=y,VAR=VAR,ENV=ENV,nIter=nIter,burnIn=burnIn,thin=thin,df=df,dfg=dfg,dfh=dfh,dfb=dfb,S=S,Sg=Sg,Sb=Sb,Sh=Sh, A=A,inits=inits,nchain=nchain,seed=seed,savedir=savedir);	
 	
 }
 
